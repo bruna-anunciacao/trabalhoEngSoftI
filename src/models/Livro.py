f2762b12e20878ab6ee256d60f931ffebc67a6fc
bruna-anunciacao <bruunaaa04@gmail.com>
2025-01-23 12:40:56 -0300

    feat: create book and user class

---
+++ src/models/Livro.py
@@ -0,0 +1,9 @@
+class Livro:
+    def __init__(self, codigo, titulo, editora, autor, edicao, ano):
+        self.codigo = codigo
+        self.titulo = titulo
+        self.editora = editora
+        self.autor = autor
+        self.edicao = edicao
+        self.ano = ano
+    
---
+++ src/models/Usuario.py
@@ -0,0 +1,5 @@
+class Usuario:
+    def __init__(self, codigo, nome, tipo):
+        self.codigo = codigo
+        self.nome = nome
+        self.tipo = tipo
